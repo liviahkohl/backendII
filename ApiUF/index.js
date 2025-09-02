47d9febd73bdce11c3a3f185bb4e019bf2917eb5
liviahkohl <analiviakohlassisdasilva@gmail.com>
2025-09-02 09:01:13 -0400

    02/09

--- ApiUF/index.js
+++ ApiUF/index.js
@@ -1,30 +1,28 @@
-import colecaoUf from "./Dados/dados.js";
 import express from 'express';
+import { buscarUfs, buscarUfPorId, buscarUfsPorNome } from "./servicos/servico.js";
 
 const app = express();
 
-app.get('/Ufs', (req, res) => {
-    res.json(colecaoUf)
-});
+app.get('/ufs', (req, res) => {
+    const nomeUf = req.query.busca;
+    const resultado = nomeUf ? buscarUfsPorNome(nomeUf) : buscarUfs();
+
+    if(resultado.length > 0) {
+        res.json(resultado);
+    } else {
+        res.status(404).send({ "erro": "Nenhuma Uf encontrada"});
+    }
+})
 
 app.get('/ufs/:iduf', (req, res) => {
-    const idUF = parseInt(req.params.iduf);
-    let mensagemErro = '';
-    let uf;
-
-    if (!(isNaN(idUF))) {
-        uf = colecaoUf.find(u => u.id === idUF);
-        if (!uf) {
-            mensagemErro = 'UF não encontrada';
-        }
-    } else {
-        mensagemErro = 'Requisição inválida'
-    }
+    const uf = buscarUfPorId(req.params.iduf);
 
     if (uf) {
         res.json(uf);
+    } else if (isNaN(parseInt(req.params.iduf))) {
+        res.status(400).send({ "erro": "Requisição inválida"});
     } else {
-        res.status(404).json({ "erro": mensagemErro })
+        res.status(404).send({ "erro": "UF não encontrada"});
     }
 });
 
