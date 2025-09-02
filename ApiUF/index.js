d72020f008c1c426aefb2ceb46d22319c302c231
liviahkohl <analiviakohlassisdasilva@gmail.com>
2025-09-02 07:06:16 -0400

    02/09

--- ApiUF/index.js
+++ ApiUF/index.js
@@ -7,11 +7,26 @@ app.get('/Ufs', (req, res) => {
     res.json(colecaoUf)
 });
 
-app.get('/ufs/:iduf' , (req, res) => {
+app.get('/ufs/:iduf', (req, res) => {
     const idUF = parseInt(req.params.iduf);
-    const uf = colecaoUf.find(u => u.id === idUF);
-    res.json(uf);
-})
+    let mensagemErro = '';
+    let uf;
+
+    if (!(isNaN(idUF))) {
+        uf = colecaoUf.find(u => u.id === idUF);
+        if (!uf) {
+            mensagemErro = 'UF não encontrada';
+        }
+    } else {
+        mensagemErro = 'Requisição inválida'
+    }
+
+    if (uf) {
+        res.json(uf);
+    } else {
+        res.status(404).json({ "erro": mensagemErro })
+    }
+});
 
 app.listen(8080, () => {
     let data = new Date()
---
+++ atividade/dados.js
@@ -0,0 +1,100 @@
+const personagens_HP = [
+    {
+        id: 1,
+        nome: 'Harry Potter'
+    },
+    {
+        id: 2,
+        nome: 'Lorde Voldemort'
+    },
+    {
+        id: 3,
+        nome: 'Hermione Granger'
+    },
+    {
+        id: 4,
+        nome: 'Ronald Weasley'
+    },
+    {
+        id: 5,
+        nome: 'Albus Dumbledore'
+    },
+    {
+        id: 6,
+        nome: 'Severus Snape'
+    },
+    {
+        id: 7,
+        nome: 'Rubeus Hagrid'
+    },
+    {
+        id: 8,
+        nome: 'Sirius Black'
+    },
+    {
+        id: 9,
+        nome: 'Minerva McGonagall'
+    },
+    {
+        id: 10,
+        nome: 'Neville Longbottom'
+    },
+    {
+        id: 11,
+        nome: 'Draco Malfoy'
+    },
+    {
+        id: 12,
+        nome: 'Fred e George Weasley'
+    },
+    {
+        id: 13,
+        nome: 'Ginny Weasley'
+    },
+    {
+        id: 14,
+        nome: 'Luna Lovegood'
+    },
+    {
+        id: 15,
+        nome: 'Remus Lupin'
+    },
+    {
+        id: 16,
+        nome: 'Peter Pettigrew'
+    },
+    {
+        id: 17,
+        nome: 'Bellatrix Lestrange'
+    },
+    {
+        id: 18,
+        nome: 'Dolores Umbridge'
+    },
+    {
+        id: 19,
+        nome: 'Dobby'
+    },
+    {
+        id: 20,
+        nome: 'James Potter (Thiago Potter em português)'
+    },
+    {
+        id: 21,
+        nome: 'Lily Potter (Lilian Potter em português)'
+    },
+    {
+        id: 22,
+        nome: 'Nymphadora Tonks (Ninfadora Tonks)'
+    },
+    {
+        id: 23,
+        nome: 'Hedwig'
+    },
+    {
+        id: 24,
+        nome: 'Arthur Weasley'
+    }
+]
+
+export default personagens_HP;
